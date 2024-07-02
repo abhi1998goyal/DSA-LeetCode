76971041c4a8d9f8b38e8ea27cec57d124cb4c38
Abhishek Goyal <abhi1998goyal@gmail.com>
2024-07-03 00:53:23 +0530

    Time: 69 ms (73.12%), Space: 18.2 MB (6.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,21 +2,15 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if not nums:
             return 0
-
-        # Initialize the neg_dp and pos_dp arrays
         neg_dp = [0] * len(nums)
         pos_dp = [0] * len(nums)
-        
-        # Initialize the first element of dp arrays
         if nums[0] < 0:
             neg_dp[0] = nums[0]
         else:
             pos_dp[0] = nums[0]
-        
-        # Initialize the maximum product with the first element
+
         mx_prod = nums[0]
 
-        # Iterate through the array
         for i in range(1, len(nums)):
             if nums[i] < 0:
                 pos_dp[i] = neg_dp[i-1] * nums[i]
@@ -24,8 +18,6 @@ class Solution:
             else:
                 pos_dp[i] = pos_dp[i-1] * nums[i] if pos_dp[i-1] != 0 else nums[i]
                 neg_dp[i] = neg_dp[i-1] * nums[i]
-
-            # Update the maximum product
             mx_prod = max(mx_prod, pos_dp[i])
 
         return mx_prod
